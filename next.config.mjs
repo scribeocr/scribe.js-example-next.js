bb8143f9de16980420591a42008d276cc528a4cf
Balearica <admin@scribeocr.com>
2024-09-21 06:32:51 -0700

    Added scribe.js example code

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
+      // Set the 'process' to undefined in the client-side bundle
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          process: 'undefined'
+        })
+      );
+    }
+    return config;
+  },
+
 };
 
 export default nextConfig;
